891bde29133f2ba6f30839de1913abffc8640dc6
figo645 <figo645@126.com>
2017-05-26 14:21:07 +0800

    adjusted the compiler and make it work thru

--- simpleapp/src/main/java/org/JavaMain.java
+++ simpleapp/src/main/java/org/JavaMain.java
@@ -1,10 +1,9 @@
-
+package org;
 public class JavaMain {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi,there")
-		System.out.println("hi,there1")
+		System.out.println("hi,there");
+		System.out.println("hi,there1");
 	}
 
 }
