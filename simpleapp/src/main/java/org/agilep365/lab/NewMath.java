988f5bbb19affecc8d1a6f1157cddeb686cdeef2
chenfei xu <figo645@126.com>
2018-10-27 13:24:27 +0800

    new update

--- simpleapp/src/main/java/org/agilep365/lab/NewMath.java
+++ simpleapp/src/main/java/org/agilep365/lab/NewMath.java
@@ -4,30 +4,38 @@ package org.agilep365.lab;
 //import java.math.*;
 //import java.awt.*;
 public class NewMath {
- public int addedValue = 1;
+	public int addedValue = 1;
 
- public int add(int x, int y) {
-  // bad smell
-  for (int i = 0; i < 100; i++) {
-   for (int j = 0; j < 100; j++) {
-    for (int k = 0; k < 100; k++) {
-     // 3 loops
-     //x = x+ y;
-    }
-   }
-  }
-  return x + y;
- }
+	/**
+	 * Add for Calculator
+	 * 
+	 * @param numbers
+	 * @return
+	 */
+	public int add(String numbers) {
+		return 0;
+	}
 
- public int minus(int x, int y) {
-  return x - y;
- }
+	public int add(int x, int y) {
+		// bad smell
+		// for (int i = 0; i < 100; i++) {
+		// for (int j = 0; j < 100; j++) {
+		// for (int k = 0; k < 100; k++) {
+		// // 3 loops
+		// //x = x+ y;
+		// }
+		return x + y;
+	}
 
- public int multiply(int x, int y) {
-  return x * y;
- }
+	public int minus(int x, int y) {
+		return x - y;
+	}
 
- public double divide(double x, double y) {
-  return x / y;
- }
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+
+	public double divide(double x, double y) {
+		return x / y;
+	}
 }
